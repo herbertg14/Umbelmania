929d0860f7d43796ab67934efb9c17e18fab8b1c
herbertg14 <herbert.gutierrez@utexas.edu>
2016-10-01 00:09:15 -0500

    finished, with comments

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 
 
 <!-- Working in the virtual environment for Umbelmania -->
-Install package virtualenv:             pip install virtualenv
-To create virtual env:                  virtualenv venv
-To activate virtual env:                source venv/Scripts/activate
-Install packages in the virtual env:    pip install -r requirements.txt
+#Install package virtualenv:             pip install virtualenv
+#To create virtual env:                  virtualenv venv
+#To activate virtual env:                source venv/Scripts/activate
+#Install packages in the virtual env:    pip install -r requirements.txt
+
+#Each Text file corresponds to moves against their stated opponents
 
 
--- app.py
+++ app.py
@@ -3,10 +3,15 @@ import json
 
 # check opponents
 def opponents():
+
+	# make a get request
 	url = "https://umbelmania.umbel.com/opponents/"
 	opponents = request.get(url)	
 
+	# if status code okay
 	if (opponents.status_code == 200):
+
+		#for each oppoent print the opponent information
 		for opponent in opponents.json():
 			for key in opponent:
 
@@ -18,12 +23,15 @@ def opponents():
 
 #check moves
 def moves():
-	url = "https://umbelmania.umbel.com/moves/"
 
+	#make a get request
+	url = "https://umbelmania.umbel.com/moves/"
 	moves = request.get(url)
 
+	#create a dictionary of the moves
 	dic = moves.json()
 
+	#print each moves with details
 	for key in dic:
 		print("")
 		print (key)
@@ -33,10 +41,12 @@ def moves():
 #re-write the moves for a specfic opponent
 def write_moves(opponent): 
 
+	#clear the file for the given opponent
 	file = opponent + ".txt"
 	print(file)
 	open(file, "w").close()
 
+	#make a post request for the specific opponent
 	url  = "https://umbelmania.umbel.com/training/"
 	headers = {'content-type': 'application/json'}
 	payload = {"player_name" : "Herbie Fully Loaded", "opponent" : opponent}
@@ -44,81 +54,190 @@ def write_moves(opponent):
 	res = request.post(url, headers = headers, json = payload)
 	match = res.json()
 
+	#open the clean file for the opponent
 	file = open(file, "w")
 
+	#copy each move into file
 	for move in range(1000):
 		match["move"] = "K"
 
 		attack = request.post(url, headers = headers, json = match)
 		match = attack.json()
 		opponentMove = match["gamestate"]["opponent_move"]
-		print(match["gamestate"]["moves_remaining"])
+		print("Moves Remaining: " + str(match["gamestate"]["moves_remaining"]))
 		file.write(opponentMove.upper() + "\n")
-		print("")
+		# print("")
 
 	file.close()
-	print(match)
 
-#fighting Pato Bajo, Jr.
-def fight_pato():
+	print("Done getting moves!")
 
-	url  = "https://umbelmania.umbel.com/training/"
+
+#find the right counter attack for specific moves
+def findMove(move):
+	counterAttack = "K"
+
+	if (move == "D"):
+		counterAttack = "C"
+
+	elif (move == "C"): 
+		counterAttack = "J"
+
+	elif (move == "J"):
+		counterAttack = "F"
+
+	elif (move == "B"):
+		counterAttack = "J"
+
+	elif (move == "F"):
+		counterAttack = "C"
+
+	elif (move == "G"):
+		counterAttack = "F"
+
+	elif (move == "I"):
+		counterAttack = "G"
+
+	elif (move == "K"):
+		counterAttack = "F"
+
+	elif (move == "E"):
+		counterAttack = "K"
+
+	elif (move == "H"):
+		counterAttack = "E"
+
+	elif (move == "A"):
+		counterAttack = "H"
+
+
+	#return the right counter attack
+	return counterAttack
+
+
+#fighting.
+def fight(opponent):
+
+	# Fight in championship mode
+	url  = "https://umbelmania.umbel.com/championship/"
 	headers = {'content-type': 'application/json'}
-	payload = {"player_name" : "Herbie Fully Loaded", "opponent" : "pato-bajo-jr"}
+	payload = {"player_name" : "Herbie Fully Loaded", "opponent" : opponent, "email" : "herbert.gutierrez@utexas.edu"}
 
 	res = request.post(url, headers = headers, json = payload)
 	match = res.json()
 
-	print(match)
+	print (match)
+	fileName = opponent + ".txt"
 
-	
+	#open opponent file
+	file = open(fileName, "r")
+
+	#go through each move of the file for a counter attack
+	for i in range(1000):
+		line = file.readline().rstrip()
+		counterAttack = findMove(line)
+		match["move"] = counterAttack
+
+		#send the request
+		attack = request.post(url, headers = headers, json = match)
+
+		#change the match
+		match = attack.json()
+
+		#print the results
+		print("#"*20)
+		print("score".ljust(15) + ":"+str(match["gamestate"]["score"]))
+		print("totalScore".ljust(15) + ":"+ str(match["gamestate"]["total_score"]))
+		print("oppenentMove".ljust(15) + ":"+ match["gamestate"]["opponent_move"])
+		print("counterattack".ljust(15) + ":"+ counterAttack)
+
+	#close the file
+	file.close()
 
 
-	# for move in range(1000):
-	# 	match["move"] = "K"
-
-	# 	attack = request.post(url, headers = headers, json = match)
-	# 	match = attack.json()
-	# 	opponentMove = match["gamestate"]["opponent_move"]
-	# 	print(opponentMove)
-	# 	file.write(opponentMove.upper() + "\n")
-	# 	print("")
-
-	# file.close()
-	# print(match)
 #main 
 def main():
 
-	opponents()
-	moves()
-	# fight_pato()
+	# prompt user to see lucadores, see moves, fight luchador
+	print ("#" * 70 )
+	print("Select from the following options or press ENTER to quit")
+	print()
+	print("1: See Luchadores")
+	print("2: See All Moves")
+	print("3: Fight a Luchador")
+	print("4: Re-write Moves")
+	print()
 
-	# write_moves("pato-bajo-jr")
+	#user input
+	start = str(input("Select option number or press ENTER to quit:"))
+	print()
 
-	# r = open('PatoBajoJr.txt', "r")
-	# for i in range(3):
-	# 	line = r.readline().rstrip()
-	# 	print(line)
-	# print ("done")
+	#see opponents
+	if (start == "1"):
+		opponents()
 
+	#see moves
+	elif (start == "2"):
+		moves()
 
-	# res = request.post(url, headers = headers, json = payload)
-	# match = res.json()
-	# match["move"] = "K"
-	# # print(match)
-	# attack = request.post(url, headers = headers, json = match)
-	# x = attack.json()
-	# x["move"] = "K"
-	# newPost = request.post(url, headers = headers, json = x)
-	# print(newPost.json())
-	# prin(attack.json())
-	# print (res.json())
+	#fight a luchador
+	elif (start == "3"):
 
-	# print (moves.headers)
-	# print (opponents.json())
-	# print(len(opponents.json()))
-	# print(opponents.status_code)
-	# r = request.post("")
+		#select luchador to fight
+		print("#" * 70)
+		print()
+		print("1: El Rey Muy Dante")
+		print("2: Pato Bajo Jr.")
+		print("3: Princesa Comico")
+		print("4: Senor Amistoso")
+		print()
+
+		#user picked opponent
+		opponent = str(input("Who would you like to fight? "))
+
+		#fight the selected opponent
+		if (opponent == "1"):
+			fight("el-rey-muy-dante")
+		elif (opponent == "2"):
+			fight("pato-bajo-jr")
+		elif (opponent == "3"):
+			fight("princesa-comico")
+		elif (opponent == "4"):
+			fight("senor-amistoso")
+		else:
+			print ("wrong input")
+
+	#re-write moves
+	elif (start == "4"):
+
+		#select luchador to rewrite moves for
+		print("#" * 70)
+		print()
+		print("1: El Rey Muy Dante")
+		print("2: Pato Bajo Jr.")
+		print("3: Princesa Comico")
+		print("4: Senor Amistoso")
+		print()
+
+		#user picked luchador
+		opponent = str(input("Which luchador's counter attack moves would you like to rewrite? "))
+
+		#rewrite selected opponent
+		if (opponent == "1"):
+			write_moves("el-rey-muy-dante")
+		elif (opponent == "2"):
+			write_moves("pato-bajo-jr")
+		elif (opponent == "3"):
+			write_moves("princesa-comico")
+		elif (opponent == "4"):
+			write_moves("senor-amistoso")
+		else:
+			print ("wrong input")
+
+	#quitting program
+	else:
+		print("Good-bye!")
 
 
 main()
+# write_moves("el-rey-muy-dante")
